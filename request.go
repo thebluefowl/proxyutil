025cbd4d6f51b40e1feafb63c1ff0b283e954c5d
Vishnu Jayadevan <vishnu@deepsource.io>
2023-09-18 09:53:52 +0530

    feat: add CloneRequest method

---
+++ request.go
@@ -0,0 +1,29 @@
+package proxyutil
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// CloneRequest clones the request.  This should not be used for
+// very large bodies.  The body is first read and the reader is
+// replaced in both the source and destination requests.
+func CloneRequest(src *http.Request) (*http.Request, error) {
+	ctx := src.Context()
+	dst := src.Clone(ctx)
+
+	b, err := io.ReadAll(src.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read request body: %w", err)
+	}
+	src.Body.Close()
+
+	src.Body = io.NopCloser(bytes.NewReader(b))
+	dst.Body = io.NopCloser(bytes.NewReader(b))
+
+	dst.ContentLength = int64(len(b))
+
+	return dst, nil
+}
